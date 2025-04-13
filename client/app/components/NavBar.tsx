42439f1af65894116aa1fa311f0e7dc541d75e6b
nineiota0 <brianho0311@gmail.com>
2025-04-13 02:37:53 -0700

    Replace navbar image

--- client/app/components/NavBar.tsx
+++ client/app/components/NavBar.tsx
@@ -1,8 +1,15 @@
+import Image from 'next/image';
 const NavBar = () => {
     return (
       <nav className="flex w-[92%] items-center items-center justify-between py-3">
-        <div className="flex items-center gap-2">
-          <div className="size-7 rounded-full bg-gradient-to-br from-violet-500 to-pink-500" />
+        <div className="flex items-center">
+        <Image
+  src="/rocket.webp"
+  alt="Rocket Icon"
+  width={30}
+  height={30}
+  className="size-13 rounded-full object-cover"
+/>
           <h1 className="text-base font-bold md:text-2xl">Quizzit</h1>
         </div>
   
@@ -12,7 +19,7 @@ const NavBar = () => {
           <li><a href="#x" className='hover:text-gray-500'>x</a> </li>
           <li><a href="#y" className='hover:text-gray-500'>y</a> </li>
         </ul>
-        
+    
         {/* <button className="w-24 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 md:w-32 dark:bg-white dark:text-black dark:hover:bg-gray-200">
           Login
         </button> */}
