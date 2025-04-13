6bfee573df1bad2bec90c63b9862776067e56a43
Thai Le <tple06203@gmail.com>
2025-04-13 14:54:53 -0700

    Remove component border

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -11,16 +11,10 @@ import { Meteors } from './components/ui/meteors';
 import ScrollToTopRocket from './components/ScrollToTopRocket';
 import FlashcardRenderer from './components/FlashcardRenderer';
 
-type TodoItem = {
-  id: number;
-  name: string;
-  description: string;
-};
-
 export default function Home({}) {
   return (
     <div className="relative h-fit overflow-hidden">
-      <div className='flex justify-center items-center flex-col border border-gray-800 bg-gray-900 px-4 py-8 shadow-xl h-max'>
+      <div className='flex justify-center items-center flex-col bg-gray-900 px-4 py-8 shadow-xl h-max'>
         <Meteors />
         <FloatingUfo />
         <NavBar />
@@ -38,30 +32,3 @@ export default function Home({}) {
     </div>
   );
 }
-
-
-
-
-// original state:
-// return (
-//   <div className="">
-//     <div>
-//       <h1 className="text-xl font-bold mb-4">Todo List</h1>
-//       {/* {todos.length > 0 ? (
-//         <ul className="space-y-2">
-//           {todos.map((todo) => (
-//             <li key={todo.id} className="bg-white p-4 rounded shadow">
-//               <h2 className="text-lg font-semibold">{todo.name}</h2>
-//               <p className="text-gray-600">{todo.description}</p>
-//             </li>
-//           ))}
-//         </ul>
-//       ) : (
-//         <p className="text-gray-500">No todos found.</p>
-//       )} */}
-//       <h2>hello world</h2>
-//     </div>
-
-
-//   </div>
-// );
